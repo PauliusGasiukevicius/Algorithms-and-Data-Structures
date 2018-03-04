df1a5b065a2fb1fba27b06fd02ac88e615c76ff2
Photon <paulius.gasiukevicius@gmail.com>
2018-03-04 09:38:41 +0200

    Update RSQ_Game.cpp

--- DP/RSQ_Game.cpp
+++ DP/RSQ_Game.cpp
@@ -3,7 +3,21 @@ using namespace std;
 #define ll long long
 #define oo LONG_MAX/2
 
-ll solve(vector<vector<ll>>&DP, vector<ll>&A, int L, int R)
+void update(vector<ll>&BIT, int c, ll val)
+{
+    for(;c<BIT.size();c+=(c&-c))
+        BIT[c]+=val;
+}
+
+ll query(vector<ll>&BIT, int c)
+{
+    ll sum=0;
+    for(;c>0;c-=(c&-c))
+        sum+=BIT[c];
+    return sum;
+}
+
+ll solve(vector<vector<ll>>&DP, vector<ll>&A,vector<ll>&BIT, int L, int R)
 {
     if(L>R)return 0;
     if(DP[L][R]!=-oo)return DP[L][R];
@@ -11,18 +25,14 @@ ll solve(vector<vector<ll>>&DP, vector<ll>&A, int L, int R)
 
     for(int i=L; i<=R; i++)
     {
-        ll pf = 0;
-        for(int k=L; k<=i; k++)pf+=A[k];
-
-        DP[L][R]=max(DP[L][R],pf - solve(DP,A,i+1,R));
+        ll pf = query(BIT,i) - query(BIT,L-1);
+        DP[L][R]=max(DP[L][R],pf - solve(DP,A,BIT,i+1,R));
     }
 
     for(int i=R; i>=L; i--)
     {
-        ll sf = 0;
-        for(int k=i; k<=R; k++)sf+=A[k];
-
-        DP[L][R]=max(DP[L][R],sf - solve(DP,A,L,i-1));
+        ll sf = query(BIT,R) - query(BIT,i-1);
+        DP[L][R]=max(DP[L][R],sf - solve(DP,A,BIT,L,i-1));
     }
     return DP[L][R];
 }
@@ -33,9 +43,10 @@ int main()
     int n;
     while(cin>>n && n)
     {
-        vector<ll>A(n);
-        for(ll&x:A)cin>>x;
-        vector<vector<ll>>DP(n,vector<ll>(n,-oo));
-        cout<<solve(DP,A,0,n-1)<<"\n";
+        vector<ll>A(n+1);
+        vector<ll>BIT(n+1);
+        for(int i=1; i<=n; i++)cin>>A[i],update(BIT,i,A[i]);
+        vector<vector<ll>>DP(n+1,vector<ll>(n+1,-oo));
+        cout<<solve(DP,A,BIT,1,n)<<"\n";
     }
 }
