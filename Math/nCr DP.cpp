d2822f1c9f463115cf393790551d59d0100fbb68
Photon <paulius.gasiukevicius@gmail.com>
2018-03-24 16:24:35 +0200

    Update nCr DP.cpp

--- Math/nCr DP.cpp
+++ Math/nCr DP.cpp
@@ -6,3 +6,12 @@ ll binom(int n, int k)
     if(C[n][k]!=0)return C[n][k];
     return C[n][k] = binom(n-1,k-1) + binom(n-1,k);
 }
+
+ll solve(int n, int k, vector<vector<ll>>&DP, vector<ll>&A, ll m)
+{
+    if(n < k)return 0;
+    if(k==0)return 1;
+    if(DP[n][k]!=-1)return DP[n][k];
+
+    return DP[n][k] = (solve(n-1,k,DP,A,m)%m + A[n]*solve(n-1,k-1,DP,A,m)%m)%m;
+}
