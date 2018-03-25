dd0484db95062bef435f65189e852b137b26ddf2
Photon <paulius.gasiukevicius@gmail.com>
2018-03-25 10:04:11 +0300

    Update Floyd.cpp

--- Graphs/Floyd.cpp
+++ Graphs/Floyd.cpp
@@ -1,4 +1,19 @@
- for(int k=0; k<n; k++)
+//Basic floyd for APSP 
+for(int k=0; k<n; k++)
         for(int i=0; i<n; i++)
             for(int j=0; j<n; j++)
                 G[i][j]=min(G[i][j],G[i][k]+G[k][j]);
+
+//Counting paths fom all pairs of nodes
+        for(int k=0; k<=n; k++)
+            for(int i=0; i<=n; i++)
+                for(int j=0; j<=n; j++)
+                M[i][j]+=M[i][k]*M[k][j];
+
+//removing cycles
+        for(int k=0; k<=n; k++)
+            if(M[k][k])
+                for(int i=0; i<=n; i++)
+                    for(int j=0; j<=n; j++)
+                        if(M[i][k] && M[k][j])
+                        M[i][j]=-1;
