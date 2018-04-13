576dc0f2e10e547be3a6977e0d7486e086c1df7d
Photon <paulius.gasiukevicius@gmail.com>
2018-04-13 11:02:21 +0300

    Update Tarjan_BiconnectedComponents.cpp

--- Graphs/Tarjan_BiconnectedComponents.cpp
+++ Graphs/Tarjan_BiconnectedComponents.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int low[100001];
 int disc[100001];
 int P[100001];
-int root = 1;
-int child = 0;
 
 void DFS(int c, vector<int>G[], vector<vector<vector<int>>>&BC, stack<vector<int>>&S)
 {
@@ -13,37 +11,31 @@ void DFS(int c, vector<int>G[], vector<vector<vector<int>>>&BC, stack<vector<int
     disc[c]=low[c]=tm++;
 
     for(int&u:G[c])
-        if(disc[u]==0)
+        if(u!=P[c])
     {
-        child++;
-        P[u]=c;
         S.push({c,u});
+
+    if(disc[u]==0)
+    {
+        P[u]=c;
         DFS(u,G,BC,S);
         low[c]=min(low[c],low[u]);
+    }
+    else if(u!=P[c])
+        low[c]=min(disc[u],low[c]);
 
-        if(low[u] >= disc[c] )
+        if(low[u] >= disc[c])
         {
             vector<vector<int>>B;
             vector<int>e = {c,u};
-            while(S.top()!=e)
-                B.push_back(S.top()),S.pop();
-            B.push_back(S.top());
-            S.pop();
-            BC.push_back(B);
-        }
-        else if(c==root && child > 1)
-        {
-            vector<vector<int>>B;
-            vector<int>e = {c,u};
-            while(S.top()!=e)
+            vector<int>e2 = {u,c};
+            while(S.top()!=e && S.top()!=e2)
                 B.push_back(S.top()),S.pop();
             B.push_back(S.top());
             S.pop();
             BC.push_back(B);
         }
     }
-    else if(u!=P[c] && disc[u] < low[c])
-        low[c]=disc[u],S.push({c,u});
 }
 
 int main()
@@ -52,38 +44,34 @@ int main()
     int n,m;
     cin>>n>>m;
     vector<int>G[100001];
+    map<pair<int,int>,int>mp;
 
     for(int i=0,u,v; i<m; i++)
-        cin>>u>>v,G[u].push_back(v),G[v].push_back(u);
+        cin>>u>>v,G[u].push_back(v),G[v].push_back(u),mp[{u,v}]=mp[{v,u}]=i+1;
 
     vector<vector<vector<int>>>BC;
 
-    for(int i=0; i<n; i++)
+    for(int i=1; i<=n; i++)
         if(disc[i]==0)
     {
-        root=i;
         stack<vector<int>>S;
         DFS(i,G,BC,S);
-        if(!S.empty())
-        {
-            vector<vector<int>>B;
-            while(!S.empty())
-                B.push_back(S.top()),S.pop();
-            BC.push_back(B);
-        }
     }
 
-    int ats[2]={};
-
+    set<int>ats;
 
     for(auto&b : BC)
     {
-        //cout<<b.size()<<"\n";
         set<int>v;
+        set<int>E;
         for(auto&e:b)
-            v.insert(e[0]),v.insert(e[1]);//,cout<<e[0]<<" "<<e[1]<<"\n";
-        ats[v.size()%2]++;
+            v.insert(e[0]),v.insert(e[1]),E.insert(mp[{e[0],e[1]}]);
+
+        if(v.size()==E.size())
+            for(auto e:E)
+            ats.insert(e);
     }
 
-    cout<<ats[0]<<" "<<ats[1]<<"\n";
+    cout<<ats.size()<<"\n";
+    for(auto x : ats)cout<<x<<" ";
 }
