6df0574e7ab94746272f1ec36c5143c03be14132
Paulius Gasiukevičius <paulius.gasiukevicius@gmail.com>
2019-06-25 08:48:52 +0300

    Update tetrahedron.cpp

--- Math/Geometry/tetrahedron.cpp
+++ Math/Geometry/tetrahedron.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 #define ll long long
 #define ld long double
-#define oo 666666666
+#define oo 1234567890
 
 //If U, V, W, u, v, w are lengths of edges of the tetrahedron
 //(first three form a triangle; u opposite to U and so on), then
-ld tetrahedronArea(ld U, ld V, ld W, ld u, ld v, ld w)
+ld tetrahedronVolume(ld U, ld V, ld W, ld u, ld v, ld w)
 {
     ld z = (W-u+v)*(u-v+W);
     ld Z = (v-W+u)*(W+u+v);
@@ -22,16 +22,30 @@ ld tetrahedronArea(ld U, ld V, ld W, ld u, ld v, ld w)
     return sqrt((-a+b+c+d)*(a-b+c+d)*(a+b-c+d)*(a+b+c-d)) / 192 / u / v / w;
 }
 
+ld heron(ld a, ld b, ld c)
+{
+    ld s = (a+b+c)/2.0;
+    return sqrt(s*(s-a)*(s-b)*(s-c));
+}
+
+ld radiusOfSphereInTetrahedron(ld WX, ld WY, ld WZ, ld XY, ld XZ, ld YZ)
+{
+    ld volume = tetrahedronVolume(XY,YZ,XZ,WZ,WX,WY);
+    ld surfaceArea = heron(WX,WY,XY) + heron(WY,WZ,YZ) + heron(WX,WZ,XZ) + heron(XY,XZ,YZ);
+    ld R = 3*volume/surfaceArea;
+    return R;
+}
+
 int main()
 {
-    ios::sync_with_stdio(0);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t;
-    ld u,v,w,U,V,W;
+    ld WX, WY, WZ, XY, XZ, YZ;
     cin>>t;
 
     while(t--)
     {
-        cin>>u>>w>>v>>V>>W>>U;
-        cout<<setprecision(4)<<fixed<<tetrahedronArea(U,V,W,u,v,w) <<"\n";
+        cin>>WX>>WY>>WZ>>XY>>XZ>>YZ;
+        cout<<setprecision(4)<<fixed<<radiusOfSphereInTetrahedron(WX,WY,WZ,XY,XZ,YZ)<<"\n";
     }
 }
